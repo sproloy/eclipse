01f020be93db6ef7d5e28bbfb436bb33db77ae4e
sproloy <proloy99@gmail.com>
2017-10-11 15:51:02 +0530

    jhj

---
+++ Artifact/Guide/guide.ps1
@@ -0,0 +1,51 @@
+﻿function Create-Folder {
+    Param ([string]$path)
+    if ((Test-Path $path) -eq $false) 
+    {
+        Write-Host "$path doesn't exist. Creating now.."
+        New-Item -ItemType "directory" -Path $path
+    }
+}
+
+function Download-File{
+    Param ([string]$src, [string] $dst)
+
+    (New-Object System.Net.WebClient).DownloadFile($src,$dst)
+    #Invoke-WebRequest $src -OutFile $dst
+}
+
+function WaitForFile($File) {
+  while(!(Test-Path $File)) {    
+    Start-Sleep -s 10;   
+  }  
+} 
+
+
+#Setup Folders
+
+#$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+
+$setupFolder= "$($env:USERPROFILE)\Desktop\guide.txt"
+Create-Folder "$setupFolder"
+
+
+
+
+
+
+# haxm 
+if((Test-Path "$setupFolder\guide.txt") -eq $false)
+{
+  
+        Download-File "https://atestdeccan6192.blob.core.windows.net/guide/guide.txt" "$setupFolder\guide.txt"
+}
+
+
+
+ 
+
+
+
+
+
