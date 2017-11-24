4e0c457de6a571baf47f8ca666772b17aa57c7c8
sproloy <proloy99@gmail.com>
2017-11-24 18:05:50 +0530

    xcmnbfdnzgedj,,jhgiuh

--- Artifact/STP Test Env/angular/angular.ps1
+++ Artifact/STP Test Env/angular/angular.ps1
@@ -1,5 +1,9 @@
 ﻿$env:Path += ";C:\Program Files\nodejs"
 npm install -g @angular/cli
+
+$Source = "C:\Windows\System32\config\systemprofile\AppData\Roaming\npm"
+$Destination = 'C:\users\*\AppData\Roaming'
+Get-ChildItem $Destination | ForEach-Object {Copy-Item -Path $Source -Destination $_ -Force -Recurse}
 #npm install -g @angular/cli -FilePath "$Env:USERPROFILE\AppData\Roaming"
 
 
