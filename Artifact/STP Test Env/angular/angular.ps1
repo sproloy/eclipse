468962135f410d42a747e7ac448eceef51493f96
sproloy <proloy99@gmail.com>
2017-11-24 17:53:44 +0530

    fsdsxgvdbbx

--- Artifact/STP Test Env/angular/angular.ps1
+++ Artifact/STP Test Env/angular/angular.ps1
@@ -1,6 +1,6 @@
 ﻿$env:Path += ";C:\Program Files\nodejs"
-#npm install -g @angular/cli
-npm install -g @angular/cli -FilePath "$Env:USERPROFILE\AppData\Roaming"
+npm install -g @angular/cli
+#npm install -g @angular/cli -FilePath "$Env:USERPROFILE\AppData\Roaming"
 
 
 #[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";C:\Windows\system32\config\systemprofile\AppData\Roaming\npm", "Machine")
